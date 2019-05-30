6e0957a482886148781a323d547e90b1d9e564e1
Andrew Liebchen <andrewliebchen@gmail.com>
2019-05-29 18:29:32 -0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "aframe";
+// import "aframe-particle-system-component";
+import { Entity, Scene } from "aframe-react";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = props => (
+  <Scene>
+    <Entity
+      geometry={{ primitive: "box" }}
+      material={{ color: "red" }}
+      position={{ x: 0, y: 0, z: -5 }}
+    />
+    {/* <Entity particle-system={{ preset: "snow" }} /> */}
+    <Entity light={{ type: "point" }} />
+    <Entity gltf-model={{ src: "virtualcity.gltf" }} />
+    <Entity text={{ value: "Hello, WebVR!" }} />
+  </Scene>
+);
 
 export default App;
